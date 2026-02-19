fe7bf0d4202c06ba2c7607550370d607046274c2
gamequang494-svg <gamequang494@gmail.com>
2026-02-19 09:33:22 +0700

    Add websocket support to app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from websockets.asyncio.server import serve
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
